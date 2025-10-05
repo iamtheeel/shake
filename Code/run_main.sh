5a5fabb06782ea360fded469732575bae0678d17
Joshua B. Mehlman <jmehlman@sfsu.edu>
2025-10-05 15:06:41 -0700

    Set up to pick the best GPU to run on

--- Code/run_main.sh
+++ Code/run_main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 # 🛠 Helps reduce fragmentation by allowing larger memory segments
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-
 export TRITON_CACHE_DIR=$HOME/.local_triton_cache
+
+
 deepspeed --num_gpus 1 main.py $@
